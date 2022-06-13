058f442c1c4daa400219bee768b5ee346d94d4b0
Mohamed Amine Themi <MAThemi@se.linedata.com>
2022-06-13 23:38:50 +0100

    updates old projects

--- src/main/java/com/funsoft/cabinet/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.funsoft.cabinet.repository;
-
-import com.funsoft.cabinet.model.Patient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PatientRepository extends CrudRepository<Patient,Long> {
-}
--- src/main/java/com/funsoft/cabinet/repository/PointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.funsoft.cabinet.repository;
-
-import com.funsoft.cabinet.model.Pointment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PointmentRepository extends CrudRepository<Pointment,Long> {
-}
