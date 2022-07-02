23301184434be9a7892b9772dcf1f82ea0c5f922
mariem3107 <mariem.ouali@esprit.tn>
2022-07-02 14:51:13 +0100

    Mariem

--- src/main/java/com/example/IBanque/model/Compte.java
+++ src/main/java/com/example/IBanque/model/Compte.java
@@ -1,5 +1,9 @@
 package com.example.IBanque.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -8,10 +12,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Inheritance(strategy= InheritanceType.TABLE_PER_CLASS)
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="")
+
 public  class Compte {
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idCompte;
     @NotBlank
     private long numCompte;
@@ -20,7 +30,7 @@ public  class Compte {
     @NotBlank
     private String typeCompte;
     @NotBlank
-    @Size(max=8)
+    @Size(max = 8)
     private long cinUser;
     @OneToMany(mappedBy = "compte")
     Set<Transaction> transactions = new HashSet<>();
@@ -32,75 +42,4 @@ public  class Compte {
     @ManyToOne
     private User user;
 
-    public long getIdCompte() {
-        return idCompte;
-    }
-
-    public void setIdCompte(long idCompte) {
-        this.idCompte = idCompte;
-    }
-
-    public long getNumCompte() {
-        return numCompte;
-    }
-
-    public void setNumCompte(long numCompte) {
-        this.numCompte = numCompte;
-    }
-
-    public Double getArgent() {
-        return argent;
-    }
-
-    public void setArgent(Double argent) {
-        this.argent = argent;
-    }
-
-    public String getTypeCompte() {
-        return typeCompte;
-    }
-
-    public void setTypeCompte(String typeCompte) {
-        this.typeCompte = typeCompte;
-    }
-
-    public long getCinUser() {
-        return cinUser;
-    }
-
-    public void setCinUser(long cinUser) {
-        this.cinUser = cinUser;
-    }
-
-    public Set<Transaction> getTransactions() {
-        return transactions;
-    }
-
-    public void setTransactions(Set<Transaction> transactions) {
-        this.transactions = transactions;
-    }
-
-    public Set<Chequier> getChequiers() {
-        return Chequiers;
-    }
-
-    public void setChequiers(Set<Chequier> chequiers) {
-        Chequiers = chequiers;
-    }
-
-    public Set<Credit> getCredits() {
-        return credits;
-    }
-
-    public void setCredits(Set<Credit> credits) {
-        this.credits = credits;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
