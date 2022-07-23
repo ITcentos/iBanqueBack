1799af66aa4d745d38a5c8ec8e1655ad4f05c4f0
MedAminethemi <themi.med.amin@gmail.com>
2022-07-23 03:19:22 +0100

    merge branch amine

--- src/main/java/com/example/IBanque/controller/DemChaquierREST.java
+++ src/main/java/com/example/IBanque/controller/DemChaquierREST.java
@@ -8,20 +8,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/v1")
 @CrossOrigin(origins = "*",allowedHeaders = "*")
 public class DemChaquierREST {
     @Autowired
     DemChequierService demChequierService;
-    @PostMapping("/demandeChaquier")
+    @PostMapping("/demandecheq")
     public DemChequier save_demande(@Valid @RequestBody DemChequier demChequier){
         return demChequierService.saveourupdate(demChequier);
     }
 
-    @GetMapping("/test")
-    public String save_doctor(){
-        return "test ok";
+    @GetMapping("/demandecheq/{id}")
+    public Optional<DemChequier> get_demande(@PathVariable(value="id") Long id ){
+
+        return  demChequierService.find_demande(id) ;
     }
+
+    @GetMapping("/alldemandecheq")
+    public List<DemChequier> get_demandes(){
+        return  demChequierService.FindAllDem() ;
+    }
+
+    @DeleteMapping("/deletedemandecheq/{id}")
+    public String delete_demande(@PathVariable(value="id") Long id ){
+
+
+        demChequierService.delete_demande(id) ;
+        return "Demande chequier num "+id +"is deleted  successfully ";
+
+    }
+
+
+
 
 }
